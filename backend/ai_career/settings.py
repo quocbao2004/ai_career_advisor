10905a630a8aca4d4a7881d7601b4fa14033dd85
Đào Quốc Bảo <daoquocbao2k04@gmail.com>
2026-01-04 12:22:52 +0700

    last

--- backend/ai_career/settings.py
+++ backend/ai_career/settings.py
@@ -156,6 +156,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# --- CẤU HÌNH WHITENOISE (Để Render phục vụ file tĩnh tốt nhất) ---
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
