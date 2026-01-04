5d40c990a83327a486740045c55770478515df66
Đào Quốc Bảo <daoquocbao2k04@gmail.com>
2026-01-04 12:57:26 +0700

    last

--- backend/ai_career/settings.py
+++ backend/ai_career/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
